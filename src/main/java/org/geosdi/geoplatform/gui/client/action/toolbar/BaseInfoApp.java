68060d3e7968ac519ab8d72c59aa11f8d3c81a4c
Giuseppe La Scaleia <glascaleia@gmail.com>
2016-10-11 15:24:25 +0200

    FIXED StartUp Base App

--- src/main/java/org/geosdi/geoplatform/gui/client/action/toolbar/BaseInfoApp.java
+++ src/main/java/org/geosdi/geoplatform/gui/client/action/toolbar/BaseInfoApp.java
@@ -37,19 +37,19 @@ package org.geosdi.geoplatform.gui.client.action.toolbar;
 
 import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.mvc.Dispatcher;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import org.geosdi.geoplatform.gui.action.ToolbarApplicationAction;
 import org.geosdi.geoplatform.gui.client.MapWidgetEvents;
 import org.geosdi.geoplatform.gui.client.Resources;
 
 /**
- *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
  * @email giuseppe.lascaleia@geosdi.org
  */
 public class BaseInfoApp extends ToolbarApplicationAction {
 
     public BaseInfoApp() {
-        super("Base App", Resources.ICONS.geoPortalInfo(), 
+        super("Base App", AbstractImagePrototype.create(Resources.ICONS.geoPortalInfo()),
                 "Basic Application");
     }
 
